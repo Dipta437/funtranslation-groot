e7397cb8cef935dfbe2703c100233f751d76c53a
Dipta437 <53457948+Dipta437@users.noreply.github.com>
2022-04-17 16:32:15 +0600

    added api's and js functionalities

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-var inputTxt = document.querySelector("#input-text");
 var btnTranslate = document.querySelector("#btn-translate");
-var outputTxt = docment.querySelector("#output-text");
+var inputTxt = document.querySelector("#input-text");
+var outputTxt = document.querySelector("#output-text");
+
+serverURL = "https://api.funtranslations.com/translate/groot.json"
+
+function getTranslationURL(text){
+    return  serverURL + "?" + "text= " + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong the server! try again after sometime")
+}
+
+function clickHandler(){
+    var inputText = inputTxt.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputTxt.innerText = translatedText;
+    })
+    .catch(errorHandler)
+}
+btnTranslate.addEventListener ("click", clickHandler);
